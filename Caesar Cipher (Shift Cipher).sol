7508f3decdf9a5e81e6406d03b00cba64f13b2a8
rajmahesh446-cell <rajmahesh446@gmail.com>
2026-01-22 11:37:08 +0530

    Create Caesar Cipher (Shift Cipher).sol

---
+++ Caesar Cipher (Shift Cipher).sol
@@ -0,0 +1,15 @@
+public class CaesarCipher {
+    public static void main(String[] args) {
+        String text = "JAVA";
+        int shift = 2;
+        String result = "";
+
+        for (char c : text.toCharArray()) {
+            // Shift character and move back to 'A' (65) if needed
+            result += (char) ((c + shift - 65) % 26 + 65);
+        }
+        
+        System.out.println("Cipher: " + result); 
+        // Output: LCXC
+    }
+}
